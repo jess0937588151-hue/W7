8e3e2a55176e32f17fac03d5f715fbe6cf78870d
jess0937588151-hue <jess0937588151@gmail.com>
2026-04-24 23:52:47 +0800

    Create PrintLine.kt

---
+++ model/PrintLine.kt
@@ -0,0 +1,8 @@
+package com.example.universalprinter.model
+
+data class PrintLine(
+    val text: String,
+    val align: Align = Align.LEFT,
+    val bold: Boolean = false,
+    val doubleSize: Boolean = false
+)
