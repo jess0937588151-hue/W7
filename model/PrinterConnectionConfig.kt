064e718ab0f17bbfc5e690e45a20c8e8fcd020f9
jess0937588151-hue <jess0937588151@gmail.com>
2026-04-24 23:53:54 +0800

    Create PrinterConnectionConfig.kt

---
+++ model/PrinterConnectionConfig.kt
@@ -0,0 +1,9 @@
+package com.example.universalprinter.model
+
+data class PrinterConnectionConfig(
+    val type: PrinterType,
+    val ip: String? = null,
+    val port: Int = 9100,
+    val bluetoothMac: String? = null,
+    val charsetName: String = "GBK"
+)
