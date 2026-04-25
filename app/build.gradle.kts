bf1206effbce1894a28a24aa5b9677542181788f
jess0937588151-hue <jess0937588151@gmail.com>
2026-04-25 17:32:12 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,21 +15,6 @@ android {
         versionName = "1.0.0"
     }
 
-    buildFeatures {
-        viewBinding = true
-        aidl = true
-    }
-
-    sourceSets {
-        getByName("main") {
-            manifest.srcFile("src/main/AndroidManifest.xml")
-            java.setSrcDirs(listOf("src/main/java", ".."))
-            java.exclude("app/**", ".github/**", "gradle/**", "build/**", "**/*.md", "**/*.kts")
-            res.srcDirs("src/main/res")
-            aidl.srcDirs("src/main/aidl")
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -40,20 +25,17 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.9.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
