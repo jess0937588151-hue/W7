1d257f02fc202eff042dbe59d0ae2b014c5cc3ad
jess0937588151-hue <jess0937588151@gmail.com>
2026-04-25 20:16:01 +0800

    Delete PrintJob.kt

--- PrintJob.kt
+++
@@ -1,9 +0,0 @@
-package com.example.universalprinter.model
-
-data class PrintJob(
-    val lines: List<PrintLine>,
-    val qrContent: String? = null,
-    val feedLines: Int = 4,
-    val cut: Boolean = true,
-    val openCashDrawer: Boolean = false
-)
